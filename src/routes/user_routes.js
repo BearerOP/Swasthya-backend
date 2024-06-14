db1aed38a19884bbdff60eb0828a8a319fcf6e66
BearerOP <ankity1892003@gmail.com>
2024-06-14 15:44:25 +0530

    user services added

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -4,18 +4,21 @@ const router = express.Router();
 const user_auth = require("../../middleware/user_auth.js");
 
 const {
-    user_register }  = require("../controllers/user_controller.js")
+  user_register,
+  user_login,
+  user_logout,
+  user_profile,
+  sendOtp
+} = require("../controllers/user_controller.js");
 
-router.get("/",user)
-
-// router.post("/login", user_login);
+router.post("/login", user_login);
 
 router.post("/register", user_register);
 
-// router.post("/logout",user_auth,user_logout)
+router.post("/logout",user_auth,user_logout)
 
-// router.post("/sendOtp",sendOtp);
+router.post("/sendOtp",sendOtp);
 
-// router.get("/profile",user_auth,user_profile);
+router.get("/profile",user_auth,user_profile);
 
 module.exports = router;
