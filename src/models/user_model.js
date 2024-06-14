1122f22bd9054a9f66b8f2030493f434843fc657
BearerOP <ankity1892003@gmail.com>
2024-06-14 14:50:09 +0530

    initial commit

---
+++ src/models/user_model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: Number, // User's weight in kilograms
+    required: true,
+  },
+  height: {
+    type: Number, // User's height in centimeters
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "transMale", "transFemale", "nonBinary"],
+  },
+  food_preference: {
+    type: String,
+    enum: ["veg", "nonVeg"],
+  },
+  auth_key: {
+    type: String,
+    default: null,
+  },
+  notificationToken: {
+    type: String,
+    default: null,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
