d4c6d876dd83db615568cde064cb8f45692a5abd
Slug-For-Sure <slugtheproject@gmail.com>
2025-02-13 03:15:44 +0530

    fix: update route paths for request and relatives endpoints

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ app.use("/Sleep", require("./src/routes/sleep_patterns_routes.js"));
 app.use("/meal", require("./src/routes/generate_meal_plan_routes.js"));
 app.use("/medication", require("./src/routes/medication_routes.js"));
 app.use("/leaderboard", require("./src/routes/leaderboard_routes.js"));
-app.use("/", require('./src/routes/request_routes.js'));
-app.use("/", require('./src/routes/relatives_routes.js'));
+app.use("/request", require('./src/routes/request_routes.js'));
+app.use("/relaties", require('./src/routes/relatives_routes.js'));
 app.use("/water", require('./src/routes/water_routes.js'));
 
 let { connectDB } = require("./db/dbconnection.js");
